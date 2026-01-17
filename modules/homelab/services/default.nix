f22571a1be81c999e2d4618e333b36e7148e3b74
zekurio <git@zekurio.me>
2026-01-17 12:18:41 +0100

    feat: replace sabnzbd with nzbget

--- modules/homelab/services/default.nix
+++ modules/homelab/services/default.nix
@@ -7,12 +7,12 @@
     ./jellyfin.nix
     ./jellyseerr.nix
     ./media-share.nix
+    ./nzbget.nix
     ./paperless-ngx.nix
     ./pocket-id.nix
     ./prowlarr.nix
     ./qbittorrent.nix
     ./radarr.nix
-    ./sabnzbd.nix
     ./sonarr.nix
   ];
 }
