7474594327216147ffb2a42f0f22b9f467afb364
zekurio <git@zekurio.me>
2026-02-14 00:38:47 +0100

    feat: simplify sahaquiel and enable auto upgrades

--- machines/nixos/sahaquiel/configuration.nix
+++ machines/nixos/sahaquiel/configuration.nix
@@ -1,10 +1,8 @@
 {
-  config,
   lib,
   modulesPath,
   ...
-}:
-{
+}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     ./disko.nix
@@ -34,9 +32,11 @@
     nftables.enable = true;
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 ];
+      allowedTCPPorts = [22];
     };
   };
+
+  modules.virtualization.enable = true;
 
   services = {
     openssh = {
@@ -46,63 +46,14 @@
         PermitRootLogin = "no";
       };
     };
-
-    pangolin = {
-      enable = true;
-      openFirewall = true;
-      baseDomain = "zekurio.xyz";
-      dashboardDomain = "edge.zekurio.xyz";
-      dnsProvider = "cloudflare";
-      letsEncryptEmail = "git@zekurio.xyz";
-      environmentFile = config.sops.secrets.pangolin_env.path;
-      settings = {
-        app.telemetry.anonymous_usage = false;
-        flags = {
-          disable_signup_without_invite = true;
-          disable_user_create_org = true;
-          require_email_verification = false;
-        };
-      };
-    };
-
-    crowdsec = {
-      enable = true;
-      settings = {
-        general.api.server.enable = true;
-        lapi.credentialsFile = "/etc/crowdsec/local_api_credentials.yaml";
-      };
-      hub.collections = [
-        "crowdsecurity/linux"
-        "crowdsecurity/sshd"
-      ];
-      localConfig.acquisitions = [
-        {
-          source = "journalctl";
-          journalctl_filter = [ "_SYSTEMD_UNIT=sshd.service" ];
-          labels.type = "syslog";
-        }
-      ];
-    };
-
-    crowdsec-firewall-bouncer = {
-      enable = true;
-      settings = {
-        mode = "nftables";
-        update_frequency = "10s";
-      };
-    };
   };
 
-  sops = {
-    defaultSopsFile = ../../../secrets/sahaquiel.yaml;
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-    secrets = {
-      pangolin_env = {
-        owner = "root";
-        group = "root";
-        mode = "0400";
-      };
-    };
+  system.autoUpgrade = {
+    enable = true;
+    flake = "github:zekurio/nix#sahaquiel";
+    dates = "daily";
+    randomizedDelaySec = "45min";
+    allowReboot = true;
   };
 
   time.timeZone = "Europe/Vienna";
