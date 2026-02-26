e6f24e84a37c8818c870915dca803b4f2dcd73e2
zekurio <git@zekurio.me>
2026-02-26 12:41:39 +0000

    fix: caddy mismatched hash

--- modules/homelab/services/caddy.nix
+++ modules/homelab/services/caddy.nix
@@ -93,7 +93,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
-        hash = "sha256-Zls+5kWd/JSQsmZC4SRQ/WS+pUcRolNaaI7UQoPzJA0=";
+        hash = "sha256-hZKTEzevrabjgZCCcoRKlqUfdDIUr89KEFJ84kyFxeg=";
       };
       globalConfig = ''
         email ${acmeEmail}
