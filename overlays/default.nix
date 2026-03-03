873925ef206102fa623131fd31d328a559c225d1
zekurio <git@zekurio.me>
2026-03-03 12:47:53 +0000

    switcheroo

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,5 @@
 {
   nixpkgs.overlays = [
     (import ./jellyfin-ffmpeg.nix)
-    (import ./navidrome.nix)
   ];
 }
--- overlays/navidrome.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  navidrome = prev.navidrome.overrideAttrs (old: {
-    env =
-      (old.env or {})
-      // {
-        CGO_CFLAGS_ALLOW = "--define-prefix";
-      };
-  });
-}
