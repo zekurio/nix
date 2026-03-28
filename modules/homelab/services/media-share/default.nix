ad375ae42d0de32b8442c99e5784f5ba7d6186c3
zekurio <git@zekurio.me>
2026-03-28 17:38:09 +0100

    feat: remove gonic

--- modules/homelab/services/media-share/default.nix
+++ modules/homelab/services/media-share/default.nix
@@ -84,7 +84,7 @@ in {
           ];
         };
       }
-      (lib.genAttrs ["gonic" "jellyfin" "radarr" "sabnzbd" "slskd" "sonarr"] (_: {
+      (lib.genAttrs ["jellyfin" "radarr" "sabnzbd" "slskd" "sonarr"] (_: {
         extraGroups = lib.mkAfter [shareGroup];
       }))
       (lib.genAttrs cfg.collaborators (_: {
