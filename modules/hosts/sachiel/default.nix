d78b25af1bc4455313d000ce616546a9957c9a4d
zekurio <git@zekurio.me>
2026-04-17 17:20:28 +0200

    laptop fixed

--- modules/hosts/sachiel/default.nix
+++ modules/hosts/sachiel/default.nix
@@ -1,6 +1,8 @@
-{
+{inputs, ...}: {
   imports = [
     ../_common/workstation
+    inputs.nixos-hardware.nixosModules.asus-battery
+    inputs.nixos-hardware.nixosModules.asus-zephyrus-ga401
     ./configuration.nix
   ];
 }
