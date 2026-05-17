7b0b20e15cebd2d071acbf2e85086e6ee3ca37c5
Michael Schwieger <git@zekurio.me>
2026-05-17 02:25:16 +0200

    chore(caddy): update plugin hash

--- modules/homelab/services/caddy/default.nix
+++ modules/homelab/services/caddy/default.nix
@@ -93,7 +93,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
-        hash = "sha256-o0zYCp3h7E0L1cgmJwNtGNgzhmZk5BAVoWQA+eL+zqc=";
+        hash = "sha256-RIqZlp7sV+Qka9stLN1NshgmvuSfUZdi4D9hP862jpQ=";
       };
       globalConfig = ''
         email ${acmeEmail}
