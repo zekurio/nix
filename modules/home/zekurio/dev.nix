37c5e566428ea4731e4efc0bc48797df6f8ba428
zekurio <git@zekurio.me>
2026-05-24 00:18:15 +0200

    feat(sachiel): optimize laptop power

--- modules/home/zekurio/dev.nix
+++ modules/home/zekurio/dev.nix
@@ -1,7 +1,16 @@
-{
+{pkgs, ...}: {
   programs.direnv = {
     enable = true;
     enableFishIntegration = true;
     nix-direnv.enable = true;
   };
+
+  programs.zellij = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  home.packages = [
+    pkgs.jujutsu
+  ];
 }
