6cf027c7e42b872ac2c5efe2de5efb4c48520947
zekurio <git@zekurio.me>
2026-06-03 01:44:15 +0200

    use alloy cache

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
       "https://cachix.cachix.org"
       "https://nixpkgs.cachix.org"
       "https://nix-community.cachix.org"
+      "https://alloy.cachix.org"
       "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
@@ -17,6 +18,7 @@
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "alloy.cachix.org-1:wXlNsjaHLyuPuGbiUb+O5C7sIzUSXqR8rMvI1DOpYVw="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     download-buffer-size = 1073741824;
@@ -66,7 +68,6 @@
     };
     alloy = {
       url = "github:zekurio/alloy";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     autoaspm = {
       url = "git+https://git.notthebe.ee/notthebee/AutoASPM";
--- modules/hosts/_common/default.nix
+++ modules/hosts/_common/default.nix
@@ -48,6 +48,7 @@ in {
         "https://cachix.cachix.org"
         "https://nixpkgs.cachix.org"
         "https://nix-community.cachix.org"
+        "https://alloy.cachix.org"
         "https://cache.garnix.io"
       ];
       trusted-public-keys = [
@@ -56,6 +57,7 @@ in {
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
         "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "alloy.cachix.org-1:wXlNsjaHLyuPuGbiUb+O5C7sIzUSXqR8rMvI1DOpYVw="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       auto-optimise-store = true;
