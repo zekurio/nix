5e4ff81f434d84639b4dd8854a9b91b6b1a5cbe4
zekurio <git@zekurio.me>
2026-06-26 11:09:37 +0200

    Enable Helium and Chromium extensions on lilith

--- modules/hosts/lilith/desktop-apps.nix
+++ modules/hosts/lilith/desktop-apps.nix
@@ -6,7 +6,6 @@
   system = pkgs.stdenv.hostPlatform.system;
 in {
   environment.systemPackages = [
-    inputs.helium.packages.${system}.default
     inputs.nix-t3code.packages.${system}.t3code-nightly
     inputs.zed.packages.${system}.default
     pkgs.feishin
