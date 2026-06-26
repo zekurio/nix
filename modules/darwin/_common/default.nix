180f49b23813d27e0262673753cc5f094b2247fa
Michael Schwieger <git@zekurio.me>
2026-06-26 22:05:24 +0200

    Refactor Darwin host setup and update docs

--- modules/darwin/_common/default.nix
+++ modules/darwin/_common/default.nix
@@ -3,7 +3,10 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  username = "zekurio";
+  homeDirectory = "/Users/${username}";
+in {
   imports = [
     ../../nixpkgs
   ];
@@ -11,47 +14,64 @@
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-darwin";
 
   system = {
-    primaryUser = "zekurio";
+    primaryUser = username;
     stateVersion = 6;
   };
 
   programs.fish.enable = true;
   environment.shells = [pkgs.fish];
-  users.users.zekurio = {
-    home = "/Users/zekurio";
+  users.knownUsers = [username];
+  users.users.${username} = {
+    uid = 501;
+    gid = 20;
+    description = "Michael";
+    home = homeDirectory;
     shell = pkgs.fish;
   };
 
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    trusted-users = [
-      "root"
-      "@admin"
-      "zekurio"
-    ];
-    substituters = [
-      "https://cache.nixos.org/"
-      "https://cache.numtide.com"
-      "https://cachix.cachix.org"
-      "https://nixpkgs.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://zekurio.cachix.org"
-      "https://cache.garnix.io"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "zekurio.cachix.org-1:QfL4gb2uCVEmSOOx4fLGDpygY1ycH5oUS1nteYTAgHc="
-      "zekurio.cachix.org-1:esutyOTeL/aict5fKEf0Zm4fHazmwGapCLfjekfEv9o="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-    auto-optimise-store = true;
+  nix = {
+    enable = false;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "@admin"
+        username
+      ];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://cache.numtide.com"
+        "https://cachix.cachix.org"
+        "https://nixpkgs.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://zekurio.cachix.org"
+        "https://cache.garnix.io"
+        "https://zed.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "zekurio.cachix.org-1:QfL4gb2uCVEmSOOx4fLGDpygY1ycH5oUS1nteYTAgHc="
+        "zekurio.cachix.org-1:esutyOTeL/aict5fKEf0Zm4fHazmwGapCLfjekfEv9o="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+      ];
+      auto-optimise-store = true;
+    };
+  };
+
+  environment.etc."pam.d/sudo_local".enable = lib.mkForce false;
+
+  nix-homebrew = {
+    enable = true;
+    enableFishIntegration = true;
+    user = username;
   };
 
   home-manager = {
@@ -60,14 +80,12 @@
     backupFileExtension = "backup";
     extraSpecialArgs = {inherit inputs;};
 
-    users.zekurio = {
+    users.${username} = {
       home = {
-        username = "zekurio";
-        homeDirectory = "/Users/zekurio";
+        inherit username homeDirectory;
       };
 
       imports = [
-        inputs.catppuccin.homeModules.catppuccin
         ../../home/zekurio
       ];
     };
