90af04b51bc981c36f3fa804d64d210beba35145
Michael Schwieger <git@zekurio.me>
2026-07-09 23:18:37 +0200

    Refactor Lilith Plasma theming to custom global designs

--- modules/hosts/lilith/desktop/theme.nix
+++ modules/hosts/lilith/desktop/theme.nix
@@ -1,66 +1,128 @@
 {
   config,
-  inputs,
   lib,
   pkgs,
   ...
 }: let
-  inherit (config.catppuccin) accent flavor;
-  flavorTitle = lib.toSentenceCase flavor;
-  accentTitle = lib.toSentenceCase accent;
-  colorScheme = "Catppuccin${flavorTitle}${accentTitle}";
-  cursorTheme = "catppuccin-${flavor}-${accent}-cursors";
-  lookAndFeelPackage = "Catppuccin-${flavorTitle}-${accentTitle}";
-  splashTheme = "${lookAndFeelPackage}-splash";
+  inherit (config.catppuccin) accent;
 
+  accentTitle = lib.toSentenceCase accent;
   wallpaperImage = ../../../../assets/wallpaper.jpg;
   faceImage = ../../../../assets/face.png;
 
-  # Latte is installed alongside the active flavor so Catppuccin-Latte-Blue is
-  # selectable from System Settings without changing the default look.
-  kdeTheme = pkgs.catppuccin-kde.override {
-    flavour = [flavor "latte"];
+  # Keep the Catppuccin package as a source for its color schemes, but do not
+  # install its global themes: they select Catppuccin cursors and Aurorae
+  # decorations instead of the Breeze/Klassy combination used here.
+  catppuccinKde = pkgs.catppuccin-kde.override {
+    flavour = ["frappe" "latte"];
     accents = [accent];
   };
   papirusFolders = pkgs.catppuccin-papirus-folders.override {
-    inherit flavor accent;
-  };
-  gtkTheme = pkgs.catppuccin-gtk.override {
-    variant = flavor;
-    accents = [accent];
+    flavor = "frappe";
+    inherit accent;
   };
 
-  kdeSettings = {
-    kdeglobals = {
-      General.ColorScheme = colorScheme;
-      Icons.Theme = "Papirus-Dark";
-      KDE.LookAndFeelPackage = lookAndFeelPackage;
-    };
-    kcminputrc.Mouse.cursorTheme = cursorTheme;
-    kscreenlockerrc.Greeter.LnFPackage = lookAndFeelPackage;
-    ksplashrc.KSplash.Theme = splashTheme;
-    kwinrc."org.kde.kdecoration2" = {
-      ButtonsOnLeft = "";
-      ButtonsOnRight = "IAX";
-      library = "org.kde.klassy";
-    };
-    plasmarc.Theme.name = "default";
+  mkDesign = {
+    flavor,
+    iconTheme,
+  }: let
+    flavorTitle = lib.toSentenceCase flavor;
+    id = "me.zekurio.catppuccin-${flavor}.desktop";
+  in {
+    inherit id;
+    defaults = pkgs.writeText "${id}-defaults" ''
+      [kdeglobals][KDE]
+      widgetStyle=Breeze
+
+      [kdeglobals][General]
+      ColorScheme=Catppuccin${flavorTitle}${accentTitle}
+
+      [kdeglobals][Icons]
+      Theme=${iconTheme}
+
+      [plasmarc][Theme]
+      name=default
+
+      [Wallpaper]
+      Image=Zekurio
+
+      [kcminputrc][Mouse]
+      cursorTheme=breeze_cursors
+
+      [kwinrc][org.kde.kdecoration2]
+      library=org.kde.klassy
+      theme=Klassy
+
+      [kscreenlockerrc][Greeter]
+      LnFPackage=org.kde.breeze.desktop
+
+      [ksplashrc][KSplash]
+      Theme=org.kde.breeze.desktop
+    '';
+    metadata = pkgs.writeText "${id}-metadata.json" (builtins.toJSON {
+      KPackageStructure = "Plasma/LookAndFeel";
+      KPlugin = {
+        Authors = [{Name = "Zekurio";}];
+        Category = "Global Themes (Plasma 6)";
+        Description = "Catppuccin ${flavorTitle} colors with Breeze and Klassy";
+        EnabledByDefault = true;
+        Id = id;
+        License = "LicenseRef-Proprietary";
+        Name = "Zekurio Catppuccin ${flavorTitle}";
+      };
+      "X-Plasma-APIVersion" = "2";
+    });
   };
+
+  designs = [
+    (mkDesign {
+      flavor = "frappe";
+      iconTheme = "Papirus-Dark";
+    })
+    (mkDesign {
+      flavor = "latte";
+      iconTheme = "Papirus-Light";
+    })
+  ];
+
+  wallpaperMetadata = pkgs.writeText "zekurio-wallpaper-metadata.json" (builtins.toJSON {
+    KPlugin = {
+      Authors = [{Name = "Zekurio";}];
+      Id = "Zekurio";
+      License = "LicenseRef-Proprietary";
+      Name = "Zekurio";
+    };
+  });
+
+  plasmaDesigns =
+    pkgs.runCommand "zekurio-plasma-designs" {
+      nativeBuildInputs = [pkgs.imagemagick];
+    } ''
+      mkdir -p "$out/share"
+      cp -r ${catppuccinKde}/share/color-schemes "$out/share/"
+
+      wallpaper="$out/share/wallpapers/Zekurio"
+      install -Dm644 ${wallpaperImage} "$wallpaper/contents/images/1920x1080.jpg"
+      install -Dm644 ${wallpaperMetadata} "$wallpaper/metadata.json"
+      install -Dm644 ${wallpaperImage} "$wallpaper/contents/screenshot.jpg"
+
+      ${lib.concatMapStringsSep "\n" (design: ''
+          theme="$out/share/plasma/look-and-feel/${design.id}"
+          install -Dm644 ${design.defaults} "$theme/contents/defaults"
+          install -Dm644 ${design.metadata} "$theme/metadata.json"
+          install -Dm644 ${wallpaperImage} "$theme/contents/previews/fullscreenpreview.jpg"
+          magick ${wallpaperImage} -thumbnail 400x225^ -gravity center -extent 400x225 "$theme/contents/previews/preview.png"
+          install -Dm644 ${pkgs.writeText "${design.id}-layout.js" ''
+            var desktopsArray = desktopsForActivity(currentActivity());
+            for (var i = 0; i < desktopsArray.length; i++) {
+                desktopsArray[i].wallpaperPlugin = "org.kde.image";
+            }
+          ''} "$theme/contents/layouts/org.kde.plasma.desktop-layout.js"
+        '')
+        designs}
+    '';
 
   ini = pkgs.formats.ini {};
-
-  # Plasma Login Manager's greeter reads kscreenlockerrc's nested
-  # [Greeter][Wallpaper][org.kde.image][General] group, which pkgs.formats.ini
-  # can only express as one section whose name embeds the KDE group nesting;
-  # the default ini generator escapes brackets, so section names pass through
-  # literally here instead.
-  loginIni = pkgs.formats.ini {mkSectionName = name: name;};
-  loginKscreenlockerrc =
-    kdeSettings.kscreenlockerrc
-    // {
-      "Greeter][Wallpaper][org.kde.image][General".Image = "file://${wallpaperImage}";
-    };
-
   accountsServiceUser = ini.generate "zekurio-accountsservice" {
     User = {
       Icon = "/var/lib/AccountsService/icons/zekurio";
@@ -68,36 +130,10 @@
     };
   };
 in {
-  catppuccin = {
-    cursors = {
-      enable = true;
-      inherit accent flavor;
-    };
-    gtk.icon = {
-      enable = true;
-      inherit accent flavor;
-    };
-  };
-
-  environment = {
-    systemPackages = [
-      kdeTheme
-      papirusFolders
-      pkgs.catppuccin-kvantum
-      gtkTheme
-    ];
-
-    # Plasma Login Manager runs as the plasmalogin user and reads KConfig from
-    # the system XDG config dirs before any user-specific files exist.
-    etc = {
-      "xdg/kdeglobals".source = ini.generate "catppuccin-kdeglobals" kdeSettings.kdeglobals;
-      "xdg/kcminputrc".source = ini.generate "catppuccin-kcminputrc" kdeSettings.kcminputrc;
-      "xdg/kscreenlockerrc".source = loginIni.generate "catppuccin-kscreenlockerrc" loginKscreenlockerrc;
-      "xdg/ksplashrc".source = ini.generate "catppuccin-ksplashrc" kdeSettings.ksplashrc;
-      "xdg/kwinrc".source = ini.generate "catppuccin-kwinrc" kdeSettings.kwinrc;
-      "xdg/plasmarc".source = ini.generate "catppuccin-plasmarc" kdeSettings.plasmarc;
-    };
-  };
+  environment.systemPackages = [
+    papirusFolders
+    plasmaDesigns
+  ];
 
   # User avatar shown on the login/lock screen, read by AccountsService.
   systemd.tmpfiles.rules = [
@@ -106,49 +142,23 @@ in {
   ];
 
   home-manager.users.zekurio = {
-    imports = [inputs.plasma-manager.homeModules.plasma-manager];
-
-    home.pointerCursor.enable = true;
-
-    programs.plasma.workspace.wallpaper = wallpaperImage;
-
-    catppuccin = {
-      inherit accent flavor;
-
-      cursors = {
-        enable = true;
-        inherit accent flavor;
-      };
-      gtk.icon = {
-        enable = true;
-        inherit accent flavor;
-      };
-      kvantum = {
-        enable = true;
-        inherit accent flavor;
-      };
-    };
-
+    # Breeze GTK follows Plasma's selected color scheme, allowing the Latte and
+    # Frappé global themes to switch light/dark application colors together.
     gtk = {
       enable = true;
-      colorScheme = "dark";
       theme = {
-        name = "catppuccin-${flavor}-${accent}-standard";
-        package = gtkTheme;
+        name = "Breeze";
+        package = pkgs.kdePackages.breeze-gtk;
       };
       gtk4.theme = {
-        name = "catppuccin-${flavor}-${accent}-standard";
-        package = gtkTheme;
+        name = "Breeze";
+        package = pkgs.kdePackages.breeze-gtk;
       };
     };
 
     qt = {
       enable = true;
       platformTheme.name = "kde";
-      style.name = "kvantum";
-      kde.settings = lib.recursiveUpdate kdeSettings {
-        kscreenlockerrc.Greeter.Wallpaper."org.kde.image".General.Image = "file://${wallpaperImage}";
-      };
     };
   };
 }
