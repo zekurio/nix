eec6f9e5d6d57acef5f87d507d58244a2c3c5f85
Michael Schwieger <git@zekurio.me>
2026-07-09 23:18:37 +0200

    Refactor Lilith Plasma theming to custom global designs

--- modules/hosts/lilith/desktop/browsers/helium.nix
+++ modules/hosts/lilith/desktop/browsers/helium.nix
@@ -4,25 +4,8 @@
   ...
 }: let
   system = pkgs.stdenv.hostPlatform.system;
-  extensionIds = [
-    "alblebhaoakdgapamjdifdfnaicpnklm" # Kagi Translate
-    "cdglnehniifkbagbbombnjghhcihifij" # Kagi Search
-    "nngceckbapebfimnlniiiahkandclblb" # Bitwarden Password Manager
-    "lppmekppnliemjclknbagdhoocikieoi" # 7TV
-  ];
 in {
   environment.systemPackages = [
     inputs.helium.packages.${system}.default
   ];
-
-  programs.chromium = {
-    enable = true;
-    extraOpts = {
-      BrowserThemeColor = "#ca9ee6";
-      ExtensionInstallAllowlist = extensionIds;
-      ExtensionInstallBlocklist = ["*"];
-      ExtensionInstallForcelist = extensionIds;
-      ExtensionInstallSources = ["https://services.helium.imput.net/*"];
-    };
-  };
 }
