9d66414f7faf6746e30b310d572fa4613224e32f
Michael Schwieger <git@zekurio.me>
2026-07-27 17:18:20 +0200

    update alloy

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     alloy = {
-      url = "github:zekurio/alloy/mastodon-embeds";
+      url = "github:zekurio/alloy";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     anvil = {
