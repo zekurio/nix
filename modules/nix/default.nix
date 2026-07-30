b1126c59f4f367f03d75e377bfdc7106bdaccbc1
zekurio <git@zekurio.me>
2026-07-31 00:47:31 +0200

    adding cache and updating alloy

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -9,6 +9,7 @@
     "https://cachix.cachix.org"
     "https://nixpkgs.cachix.org"
     "https://nix-community.cachix.org"
+    "https://zekurio.cachix.org"
     "https://cache.garnix.io"
   ];
   trusted-public-keys = [
@@ -17,6 +18,7 @@
     "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
     "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "zekurio.cachix.org-1:mv0mACvSLZtBkXXh5YDPPXmFBJ/eO+VkSzep6LJZrAg="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
 
