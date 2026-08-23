16d7af2f02783ead0a2143b410fce7a923fd10b9
Michael Schwieger <git@zekurio.me>
2026-08-23 22:06:23 +0200

    fix(anvil): prepare converted handoff paths

--- modules/homelab/services/media-share/default.nix
+++ modules/homelab/services/media-share/default.nix
@@ -38,6 +38,9 @@
       "${usenetDownloadsDir}/complete/radarr"
       "${usenetDownloadsDir}/complete/sonarr"
       "${usenetDownloadsDir}/complete/slskd"
+      "${usenetDownloadsDir}/converted"
+      "${usenetDownloadsDir}/converted/radarr"
+      "${usenetDownloadsDir}/converted/sonarr"
       "${usenetDownloadsDir}/incomplete"
       "${usenetDownloadsDir}/incomplete/slskd"
     ];
