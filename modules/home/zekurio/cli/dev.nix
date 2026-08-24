2f2af26bb8446848572c05cedc8444a171256673
Michael Schwieger <git@zekurio.me>
2026-08-24 23:06:58 +0200

    fix(lilith): restore Helium extension installation

--- modules/home/zekurio/cli/dev.nix
+++ modules/home/zekurio/cli/dev.nix
@@ -1,27 +1,9 @@
 {
-  flake.modules.homeManager.zekurio = {pkgs, ...}: let
-    copyCommand =
-      if pkgs.stdenv.hostPlatform.isDarwin
-      then "pbcopy"
-      else "${pkgs.wl-clipboard}/bin/wl-copy";
-  in {
+  flake.modules.homeManager.zekurio = {
     programs.direnv = {
       enable = true;
       enableFishIntegration = true;
       nix-direnv.enable = true;
     };
-
-    programs.zellij = {
-      enable = true;
-      enableFishIntegration = false;
-      settings = {
-        show_startup_tips = false;
-        support_kitty_keyboard_protocol = true;
-        copy_command = copyCommand;
-        copy_clipboard = "system";
-      };
-    };
-
-    catppuccin.zellij.enable = true;
   };
 }
