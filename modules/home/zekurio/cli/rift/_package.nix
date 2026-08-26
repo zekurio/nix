1092cda260edb9a3229ae9f79f1e54aa0cbc7414
zekurio <git@zekurio.me>
2026-08-26 21:31:24 +0200

    feat: update homelab services and lilith storage

--- modules/home/zekurio/cli/rift/_package.nix
+++ modules/home/zekurio/cli/rift/_package.nix
@@ -3,13 +3,24 @@
   lib,
   stdenvNoCC,
 }:
-stdenvNoCC.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation (finalAttrs: let
+  release = builtins.getAttr stdenvNoCC.hostPlatform.system {
+    aarch64-darwin = {
+      target = "aarch64-apple-darwin";
+      hash = "sha256-92c6lb2AlXjnyq/YMrQmp9ycy57rLwWIw8APrUTBWSQ=";
+    };
+    x86_64-linux = {
+      target = "x86_64-unknown-linux-gnu";
+      hash = "sha256-wcH9D65jvgBphDWqbeXssk+89iksJijk0OABgJ9ruZ0=";
+    };
+  };
+in {
   pname = "rift";
   version = "0.0.10";
 
   src = fetchurl {
-    url = "https://github.com/anomalyco/rift/releases/download/v${finalAttrs.version}/rift-v${finalAttrs.version}-aarch64-apple-darwin.tar.gz";
-    hash = "sha256-92c6lb2AlXjnyq/YMrQmp9ycy57rLwWIw8APrUTBWSQ=";
+    url = "https://github.com/anomalyco/rift/releases/download/v${finalAttrs.version}/rift-v${finalAttrs.version}-${release.target}.tar.gz";
+    inherit (release) hash;
   };
 
   unpackPhase = ''
@@ -31,6 +42,9 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     homepage = "https://github.com/anomalyco/rift";
     license = lib.licenses.mit;
     mainProgram = "rift";
-    platforms = ["aarch64-darwin"];
+    platforms = [
+      "aarch64-darwin"
+      "x86_64-linux"
+    ];
   };
 })
