9bacf1e50f3dc714cad4fb579d606f08aa5895ee
zekurio <git@zekurio.me>
2026-09-22 01:35:37 +0200

    fix(flake): remove unsupported GitHub shallow parameter

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
   };
 
   inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable?shallow=true";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs-unstable";
